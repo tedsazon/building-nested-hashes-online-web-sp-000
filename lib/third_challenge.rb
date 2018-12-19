426ad5585f9098d86aa1fd59a9ec748834516775
Ted Sazon <tedsazon@gmail.com>
2018-12-19 20:18:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,23 +10,29 @@ def third_challenge
         :age "54"
         },
       :hero => {
-      
+        :name "Romeo"
+        :age "15"
+        :status "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        
+        :name "Lord Capulet"
+        :age "50"
         },
       :matriarch => {
-        
+        :name "Lady Capulet"
+        :age "51"
         },
       :heroine => {
-      
+        :name "Juliet"
+        :age "15"
+        :status "alive"
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
