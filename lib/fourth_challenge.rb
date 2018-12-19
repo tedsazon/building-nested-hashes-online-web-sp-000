0fa9ead758c49ea08339c3f8a2e849e6129a8431
Ted Sazon <tedsazon@gmail.com>
2018-12-19 20:26:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,17 +8,17 @@ def fourth_challenge
                          {name: "Mercutio", age: "18", attitude: "hot-headed"}
         #your hashes here!
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
+      :heroine_friends => [ {name: "Steven", age: "30", attitude: "confused"}
+                            {name: "Nurse", age: "44", attitude: "worried"} 
       ]
    }
 }
 
-  
+
 
 end
